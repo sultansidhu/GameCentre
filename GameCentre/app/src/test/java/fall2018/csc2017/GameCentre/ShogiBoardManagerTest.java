1bc181389db01de310f3b088b92ac6290dfa850c
kapoorab <abhishek.kapoor@mail.utoronto.ca>
2018-11-18 15:52:12 -0500

    "Create ShogiBoardManagerTest file, which contains JUnitTests for ShogiBoardManager class"

---
+++ GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/ShogiBoardManagerTest.java
@@ -0,0 +1,48 @@
+package fall2018.csc2017.GameCentre;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ShogiBoardManagerTest {
+
+    @Test
+    public void getBoard() {
+    }
+
+    @Test
+    public void puzzleSolved() {
+    }
+
+    @Test
+    public void isValidTap() {
+    }
+
+    @Test
+    public void isValidTap1() {
+    }
+
+    @Test
+    public void touchMove() {
+    }
+
+    @Test
+    public void touchMove1() {
+    }
+
+    @Test
+    public void inSameRow() {
+    }
+
+    @Test
+    public void inSameCol() {
+    }
+
+    @Test
+    public void tileBlockingRow() {
+    }
+
+    @Test
+    public void tileBlockingCol() {
+    }
+}
