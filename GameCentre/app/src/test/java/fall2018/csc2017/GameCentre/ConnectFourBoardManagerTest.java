1c0430514d5b617e096a30b02a0483b20dacef71
Shahzil <shahzil.lakhani@mail.utoronto.ca>
2018-11-20 08:56:55 -0500

    Addition of many unittests for Connect 4 Board Manager including switchplayer, puzzlesolved, checksides, numRight and numLeft.

--- GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/ConnectFourBoardManagerTest.java
+++ GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/ConnectFourBoardManagerTest.java
@@ -16,18 +16,31 @@ public class ConnectFourBoardManagerTest {
 
     @Test
     public void switchPlayer() {
+        ConnectFourBoardManager bm = makeEarlyBoard();
+        int currPlayer = bm.getCurrentPlayer(18);
+        bm.touchMove(12);
+        int newPlayer = bm.getCurrentPlayer(12);
+        assertNotEquals(currPlayer, newPlayer);
+        bm.touchMove(6);
+        int newnewPlayer = bm.getCurrentPlayer(6);
+        assertEquals(currPlayer, newnewPlayer);
     }
 
     @Test
     public void puzzleSolved() {
+        ConnectFourBoardManager bm = makeEarlyBoard();
+        assertEquals(false, bm.puzzleSolved(30));//Not 4 in a row
+        assertEquals(false, bm.puzzleSolved(0));//Empty tiles all around
+        bm = makeRedOneMoveAwayBoard();
+        assertEquals(false, bm.puzzleSolved(34));//3 in a diagonal row for red
+        assertEquals(false, bm.puzzleSolved(31));//3 in a vertical row
+        bm = makeRedWinVerticalBoard();
+        assertEquals(true, bm.puzzleSolved(31));//red vertical win TODO
+        assertEquals(true, bm.puzzleSolved(20));//Red diagonal win.
     }
-
-    @Test
-    public void puzzleSolved1() {
-    }
-
     @Test
     public void checkDiagonals() {
+
     }
 
     @Test
@@ -48,11 +61,19 @@ public class ConnectFourBoardManagerTest {
 
     @Test
     public void checkSides() {
+        ConnectFourBoardManager bm = makeEarlyBoard();
+        assertEquals(false, bm.checkSides(18));//Empty on right side
+        assertEquals(false, bm.checkSides(26));//Various colors on the right
+        bm = makeRedWinVerticalBoard();
+        assertEquals(false, bm.checkSides(13));//Blanks on either side
+        bm = makeRedWinHorizontal();
+        assertEquals(true, bm.checkSides(33));
     }
 
     @Test
     public void numLeft() {
         ConnectFourBoardManager bm = makeRedOneMoveAwayBoard();
+        //TODO: Fix numLeft since it only returns 0
         assertEquals(1, bm.numLeft(2, 19));//1 black to the left
         assertEquals(0, bm.numLeft(2, 18));//at the absolute left
         assertEquals(2, bm.numLeft(1, 34));//different colors to the left
@@ -61,9 +82,11 @@ public class ConnectFourBoardManagerTest {
     @Test
     public void numRight() {
         ConnectFourBoardManager bm = makeRedOneMoveAwayBoard();
-        assertEquals(2, bm.numRight(1, 30));//multiple colors right
+        //TODO: Fix NumRight since it only seems to return 0
+        assertEquals(1, bm.numRight(1, 30));//multiple colors right
         assertEquals(0, bm.numRight(2, 35));//absolute right
         assertEquals(0, bm.numRight(1, 21));//Blanks at right
+        assertEquals(1, bm.numRight(2, 20) );//Only black on right
 
     }
 
@@ -84,6 +107,11 @@ public class ConnectFourBoardManagerTest {
 
     @Test
     public void checkEmptyTile() {
+        ConnectFourBoardManager bm = makeRedWinVerticalBoard();
+        assertEquals(true, bm.checkEmptyTile(12));//Empty tile directly above another
+        assertEquals(true, bm.checkEmptyTile(5));//Empty tile in middle of nowhere
+        assertEquals(false, bm.checkEmptyTile(20));//Red tile
+        assertEquals(false, bm.checkEmptyTile(32));//Black tile
     }
 
     @Test
@@ -160,6 +188,17 @@ public class ConnectFourBoardManagerTest {
         bm.touchMove(19);
         bm.touchMove(35);
         bm.touchMove(13);
+        return bm;
+    }
+    public ConnectFourBoardManager makeRedWinHorizontal(){
+        ConnectFourBoardManager bm = new ConnectFourBoardManager(6);
+        bm.touchMove(30);
+        bm.touchMove(24);
+        bm.touchMove(31);
+        bm.touchMove(25);
+        bm.touchMove(32);
+        bm.touchMove(26);
+        bm.touchMove(33);
         return bm;
     }
 }
