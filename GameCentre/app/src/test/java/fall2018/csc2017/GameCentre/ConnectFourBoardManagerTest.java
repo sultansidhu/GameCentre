1dfe9e4d926f99d535e0edc30ebd618830b3944a
lakhan77 <shahzil.lakhani@mail.utoronto.ca>
2018-11-19 20:22:19 -0500

    Create and add tests for Connect4BoardManager, including checkUnder, numLeft, numRight, getBoard, isvalidtap, check underneath. Remove typo.

--- GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/ConnectFourBoardManagerTest.java
+++ GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/ConnectFourBoardManagerTest.java
@@ -10,7 +10,7 @@ public class ConnectFourBoardManagerTest {
 
     @Test
     public void getBoard() {
-        ConnectFourBoardManager bm = makeEarlyBoad();
+        ConnectFourBoardManager bm = makeEarlyBoard();
         assertNotNull(bm.getBoard());
     }
 
@@ -52,18 +52,34 @@ public class ConnectFourBoardManagerTest {
 
     @Test
     public void numLeft() {
+        ConnectFourBoardManager bm = makeRedOneMoveAwayBoard();
+        assertEquals(1, bm.numLeft(2, 19));//1 black to the left
+        assertEquals(0, bm.numLeft(2, 18));//at the absolute left
+        assertEquals(2, bm.numLeft(1, 34));//different colors to the left
     }
 
     @Test
     public void numRight() {
+        ConnectFourBoardManager bm = makeRedOneMoveAwayBoard();
+        assertEquals(2, bm.numRight(1, 30));//multiple colors right
+        assertEquals(0, bm.numRight(2, 35));//absolute right
+        assertEquals(0, bm.numRight(1, 21));//Blanks at right
+
     }
 
     @Test
     public void checkUnder() {
+        ConnectFourBoardManager bm = makeRedWinVerticalBoard();
+        assertEquals(true, bm.checkUnder(13));
+        assertEquals(false, bm.checkUnder(14));
     }
 
     @Test
     public void isValidTap() {
+        ConnectFourBoardManager bm = makeEarlyBoard();
+        assertEquals(false, bm.isValidTap(30));//Already occupied position
+        assertEquals(false, bm.isValidTap(0));//Too high
+        assertEquals(true, bm.isValidTap(12));//Just right
     }
 
     @Test
@@ -72,18 +88,18 @@ public class ConnectFourBoardManagerTest {
 
     @Test
     public void checkUnderneath() {
+        ConnectFourBoardManager bm = makeRedOneMoveAwayBoard();
+        assertEquals(false, bm.checkUnderneath(0));//Not underneath
+        assertEquals(true,  bm.checkUnderneath(12));//Is underneath
     }
 
     @Test
     public void gameDrawn() {
     }
 
-    @Test
-    public void touchMove() {
-    }
 
-    public ConnectFourBoardManager makeEarlyBoad(){
-        Integer[] moveList = {30, 24, 31, 18, 25, 32, 34, 26, 20, 33, 27, 21, 19};
+    public ConnectFourBoardManager makeEarlyBoard(){
+        //Integer[] moveList = {30, 24, 31, 18, 25, 32, 34, 26, 20, 33, 27, 21, 19, 35};
         ConnectFourBoardManager bm = new ConnectFourBoardManager(6);
         bm.touchMove(30);
         bm.touchMove(24);
@@ -105,7 +121,6 @@ public class ConnectFourBoardManagerTest {
         bm.touchMove(33);
         bm.touchMove(27);
         bm.touchMove(21);
-        bm.touchMove(35);
         bm.touchMove(19);
         return bm;
     }
