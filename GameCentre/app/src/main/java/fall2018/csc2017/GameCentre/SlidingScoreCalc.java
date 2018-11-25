a87bdbda96a007347e121e63c79e6c0ea3858dd9
timapakfetrat <fatima.pakfetrat@mail.utoronto.ca>
2018-11-25 16:43:52 -0500

    Modify score algorithm of Sliding

--- GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/SlidingScoreCalc.java
+++ GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/SlidingScoreCalc.java
@@ -14,10 +14,12 @@ class SlidingScoreCalc
 
         double time = user.getTotalTime();
         int numMoves = user.getNumMoves(0);
-        if (time == 0) {
+        if (time == 0)
+        {
             return 0;
         } else {
-            return (int) Math.round(20000 * ((1 / numMoves) + 100000 / (time)));
+            System.out.println("THE TIME IS LOL: "+time);
+            return (int)(2000 * ((1 / numMoves) + 10000 / (time)));
         }
     }
 }
