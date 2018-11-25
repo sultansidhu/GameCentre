000cc0f0a7c71494aa1138fb1b0fbbdb3e95bfde
lakhan77 <shahzil.lakhani@mail.utoronto.ca>
2018-11-25 16:44:20 -0500

    Major bug fix for Shogi; Fix the bug where the guest wins and the numMoves call is upon an empty stack; Update of classes to fix this bug

--- GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/ShogiScoreCalc.java
+++ GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/ShogiScoreCalc.java
@@ -1,5 +1,7 @@
 package fall2018.csc2017.GameCentre;
 
+import java.util.HashMap;
+
 class ShogiScoreCalc {
     private FileManager fm;
     private LoginManager lm;
@@ -9,16 +11,25 @@ class ShogiScoreCalc {
     }
 
     public int calculateUserScore(User user) {
-        int numMoves = user.getNumMoves(1);
+        int numMoves;
+        Board board;
+        if(!(lm.getPersonLoggedIn().equals(user.getUsername()))){
+            HashMap<String, User> hm = fm.readObject();
+            assert hm != null;
+            User user1 = hm.get(lm.getPersonLoggedIn());//Gets the person logged in
+            System.out.println("The username seen by shogiscorecalc is: "+user1.getUsername());
+            numMoves = user1.getNumMoves(1) - 1;//p2 requires 1 more move to win
+            board = user1.getGameStack(1).peek();
+        }
+        else {
+            numMoves = user.getNumMoves(1);
+            board = user.getGameStack(1).peek();
+        }
         System.out.println("Number of moves until game ended: "+numMoves);
-        Board board = user.getGameStack(1).peek();
         int pieceDiff = board.numBlacks() - board.numReds();
         if (pieceDiff < 0){
             pieceDiff *= -1;
         }
-//        if (!(lm.getPersonLoggedIn() == user.getUsername())){
-//            pieceDiff *= -1; //If the user is not the one logged in, he is player 2
-//        }
         System.out.println("TOOK MOVES: "+numMoves);
         if(numMoves < 14){
             return 860 + (20 * pieceDiff);
