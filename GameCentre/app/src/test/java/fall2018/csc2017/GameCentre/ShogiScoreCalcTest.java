ba35fd86ecb482b24f49e016f20c4436d9546a95
kapoorab <abhishek.kapoor@mail.utoronto.ca>
2018-11-29 17:28:37 -0500

    "add ShogiScoreCalcTest class to test functions in ShogiScore; improve code coverage"

--- GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/ShogiScoreCalcTest.java
+++ GameCentre/app/src/test/java/fall2018/csc2017/GameCentre/ShogiScoreCalcTest.java
@@ -1,11 +1,50 @@
-//package fall2018.csc2017.GameCentre;
-//
-//import org.junit.Test;
-//
-//import static org.junit.Assert.*;
-//
-//import android.content.Context;
-//import org.junit.Test;
+package fall2018.csc2017.GameCentre;
+
+ import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import android.content.Context;
+import org.junit.Test;
+
+public class ShogiScoreCalcTest
+{
+
+ @Test
+ public void testGenerateScore()
+ {
+   ShogiScore s = new ShogiScore();
+   assertEquals(960, s.generateScore(13,5));
+   assertEquals(838, s.generateScore(16,4));
+   assertEquals(0, s.generateScore(800, 1));
+ }
+
+ @Test
+ public void testGetPieceDiff()
+ {
+     ShogiBoardManager bm = new ShogiBoardManager(6);
+
+     bm.setTileSelected(0);
+     bm.touchMove(12);
+     bm.setTileSelected(31);
+     bm.touchMove(13);
+     bm.setTileSelected(2);
+     bm.touchMove(8);
+     bm.setTileSelected(32);
+     bm.touchMove(14);
+     bm.setTileSelected(3);
+     bm.touchMove(15);
+
+
+     ShogiScore s = new ShogiScore();
+     assertEquals(2, s.getPieceDiff(bm.getBoard()));
+
+ }
+
+
+
+}
+
 //import org.junit.runner.RunWith;
 //import org.mockito.Mock;
 //import org.mockito.Mockito;
@@ -64,3 +103,5 @@
 //        return bm;
 //    }
 //}
+
+
