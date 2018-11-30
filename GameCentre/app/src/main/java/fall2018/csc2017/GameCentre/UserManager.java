da0bcde8fc59bea88d44b74215fb23164a2eb23f
Shahzil <shahzil.lakhani@mail.utoronto.ca>
2018-11-30 05:00:09 -0500

    Addition of comments and removal of dispensible code; remove commented out code; add JavaDoc.

--- GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/UserManager.java
+++ GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/UserManager.java
@@ -1,17 +1,28 @@
 package fall2018.csc2017.GameCentre;
 
-import android.content.Context;
 
 import java.util.Stack;
 
 class UserManager {
 
+    /**
+     * The FileManager attribute of this class
+     */
     private FileManager fm;
 
+    /**
+     * Constructor for the userManager, initalizing the FileManager.
+     */
     UserManager() {
         fm = new FileManager();
     }
 
+    /**
+     *
+     * @param username the username of the player who wishes to undo
+     * @param gameIndex the game for which the player wishes to undo
+     * @return whether or not the attempted undo was successful.
+     */
     boolean processUndo(String username, int gameIndex) {
         User user = fm.getUser(username);
         Stack<Board> userStack = fm.getStack(username, gameIndex);
@@ -26,6 +37,11 @@ class UserManager {
         }
     }
 
+    /**
+     *
+     * @param gameIndex The gameindex for which we want to add an opponent for
+     * @param opponent The string representing the opponent to face.
+     */
     void addOpponent(int gameIndex, String opponent) {
         LoginManager lm = new LoginManager();
         String username = lm.getPersonLoggedIn();
@@ -34,6 +50,13 @@ class UserManager {
         fm.saveUser(user, username);
     }
 
+    /**
+     *
+     * @param username The username for whom we want to set the undos
+     * @param undoLimit the proposed undoLimit
+     * @param gameIndex  the game for which we want to set the undos
+     * @param boardManager the boardmanager to which we will add the initial state.
+     */
     void setUserUndos(String username, int undoLimit, int gameIndex, BoardManager boardManager) {
         User user = fm.getUser(username);
         user.setAvailableUndos(gameIndex, undoLimit);
