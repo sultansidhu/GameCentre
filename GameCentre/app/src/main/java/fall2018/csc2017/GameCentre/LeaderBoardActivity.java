279f6365008087a31c47423f4629071feee5e6ff
sultansidhu <sultansinghsidhu@gmail.com>
2018-11-30 02:39:24 -0500

    Add Javadoc and delete dead code

--- GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/LeaderBoardActivity.java
+++ GameCentre/app/src/main/java/fall2018/csc2017/GameCentre/LeaderBoardActivity.java
@@ -6,18 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.io.File;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 
 public class LeaderBoardActivity extends AppCompatActivity {
-    private FileManager fm = new FileManager(); // getApplicationContext()
+    //private FileManager fm = new FileManager();
     private ScoreboardController scon = new ScoreboardController();
 
     @Override
@@ -70,54 +62,4 @@ public class LeaderBoardActivity extends AppCompatActivity {
         Intent intent = new Intent(this, GameListActivity.class);
         startActivity(intent);
     }
-
-
-    /////////////////////////LOGIC STUFF BELOW, MOVE IT OUT/////////////////////////////////////////
-
-//    private StringBuilder formatScores(ArrayList c4Scores, int numToDisplay) {
-//        StringBuilder scoreFormat = new StringBuilder();
-//        for(int i = 0; i < numToDisplay && i < c4Scores.size(); i++){
-//            Object[] scoreTup = (Object[])c4Scores.get(i);
-//            scoreFormat.append(scoreTup[0] + ": " + scoreTup[1] + "\n");//Appends the username and score to the tup.
-//        }
-//        if(scoreFormat.length() == 0){
-//            scoreFormat.append("No one to show! Be the first one!");
-//        }
-//        return scoreFormat;
-//    }
-
-//    public ArrayList getGlobalScores(int gameIndex) {
-//
-//        HashMap<String, User> users = fm.readObject();
-//        ArrayList highScoresTup = new ArrayList();
-//        Iterator it = users.entrySet().iterator();
-//        while (it.hasNext()) {
-//            HashMap.Entry pair = (HashMap.Entry) it.next();
-//            System.out.println(pair.getKey() + " = " + pair.getValue());
-//            User u = (User) pair.getValue();
-//            ArrayList<Integer> highScoresList = u.getHighestScoresList(gameIndex);
-//            for (int i = 0; i < highScoresList.size(); i++){
-//                Object[] tempTup = new Object[2];
-//                tempTup[0] = u.getUsername();
-//                tempTup[1] = highScoresList.get(i);
-//                highScoresTup.add(tempTup);
-//            }
-//
-//        }
-//        return sortListofUserandScores(highScoresTup);
-//    }
-
-//    private ArrayList sortListofUserandScores(ArrayList highScoresTup) {
-//        Collections.sort(highScoresTup, new Comparator() {
-//            @Override
-//            public int compare(Object o, Object t) {
-//                Object[] tempO = (Object[]) o;
-//                Object[] tempT = (Object[]) t;
-//                int o1 = (int) tempO[1];
-//                int t1 = (int) tempT[1];
-//                return t1 - o1;
-//            }
-//        });
-//        return highScoresTup;
-//    }
 }
